b27eaf0c2d6ad6c802ff031524c8ca1e9eaedde1
HeyChobe <00204119@uca.edu.sv>
2023-12-01 02:27:14 -0600

    :beer: Add final absolute example

--- main.c
+++ main.c
@@ -1,4 +1,26 @@
+#include <stdio.h>
+
 int main() {
-   int n = 5;
-   return n;
+    int suma = 0;
+    int i = 1;
+    int condicion = 1;
+
+    // Ciclo while para sumar los primeros 5 números naturales
+    while (i == 6) {
+        suma = suma + i;
+        i = i + 1;
+    }
+
+    if(suma == 0 && i == 4 || condicion == 4){
+        condicion = 2;
+    }else{
+        condicion = 3;
+    }
+
+    // Verifica si la suma es par sin utilizar el operador %
+    condicion = suma / 2 * 2;
+    condicion = condicion - suma;
+
+    // Devuelve 1 si la suma es par, 0 si es impar
+    return condicion;
 }
